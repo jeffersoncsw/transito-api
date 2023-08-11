52896aa3d5df2ab7762f5c7b28683cc0fdc371b0
jeffersoncsw <je_wiltenburg@hotmail.com>
2023-08-11 18:11:34 -0300

    Creating transit project

---
+++ src/main/java/com/jwiltenburg/transito/TransitoApiApplication.java
@@ -0,0 +1,13 @@
+package com.jwiltenburg.transito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransitoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransitoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jwiltenburg/transito/TransitoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jwiltenburg.transito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransitoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
