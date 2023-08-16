d4f5e2cb31fb5f5b8114028205f5d260ca3751bb
jeffersoncsw <je_wiltenburg@hotmail.com>
2023-08-16 20:49:21 -0300

    Creating the ProprietarioRepository interface in the domain layer

---
+++ src/main/java/com/jwiltenburg/transito/domain/repository/ProprietarioRepository.java
@@ -0,0 +1,9 @@
+package com.jwiltenburg.transito.domain.repository;
+
+import com.jwiltenburg.transito.domain.model.Proprietario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProprietarioRepository extends JpaRepository<Proprietario, Long> {
+}
