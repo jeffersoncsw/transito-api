23f7f37034ed3060a096f0729d78d436e018232f
“jeffersoncsw” <je_wiltenburg@hotmail.com>
2023-08-24 18:48:45 -0300

    Implementing a handler for exception business rules

--- src/main/java/com/jwiltenburg/transito/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/jwiltenburg/transito/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.jwiltenburg.transito.api.exceptionhandler;
 
+import com.jwiltenburg.transito.domain.exception.BusinessRuleException;
 import com.jwiltenburg.transito.domain.exception.ResourceNotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpHeaders;
@@ -19,6 +20,18 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     public final ResponseEntity<?> resourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
         HttpStatus status = HttpStatus.NOT_FOUND;
+
+        var problemMessage = new ExceptionMessageResource();
+        problemMessage.setStatus(status.value());
+        problemMessage.setDataHora(OffsetDateTime.now());
+        problemMessage.setTitulo(ex.getMessage());
+
+        return handleExceptionInternal(ex, problemMessage, new HttpHeaders(), status, request);
+    }
+
+    @ExceptionHandler(BusinessRuleException.class)
+    public final ResponseEntity<?> businessRuleException(BusinessRuleException ex, WebRequest request) {
+        HttpStatus status = HttpStatus.BAD_REQUEST;
 
         var problemMessage = new ExceptionMessageResource();
         problemMessage.setStatus(status.value());
