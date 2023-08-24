fef8e69992a189c5204792582ba8742d96d9ad2f
“jeffersoncsw” <je_wiltenburg@hotmail.com>
2023-08-24 19:57:48 -0300

    Creating the VeiculoRepository interface in the domain layer

---
+++ src/main/java/com/jwiltenburg/transito/domain/repository/VeiculoRepository.java
@@ -0,0 +1,9 @@
+package com.jwiltenburg.transito.domain.repository;
+
+import com.jwiltenburg.transito.domain.model.Veiculo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VeiculoRepository extends JpaRepository<Veiculo, Long> {
+}
