98d64231ee301d4a32e4db6c95bdce26a3ce396c
“jeffersoncsw” <je_wiltenburg@hotmail.com>
2023-08-29 17:11:04 -0300

    Implementing the feature to list all vehicles with pagination

--- src/main/java/com/jwiltenburg/transito/domain/service/VeiculoService.java
+++ src/main/java/com/jwiltenburg/transito/domain/service/VeiculoService.java
@@ -2,8 +2,11 @@ package com.jwiltenburg.transito.domain.service;
 
 import com.jwiltenburg.transito.api.controller.data.request.VeiculoRequest;
 import com.jwiltenburg.transito.api.controller.data.response.VeiculoResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 public interface VeiculoService {
     VeiculoResponse salvar(VeiculoRequest request);
+    Page<VeiculoResponse> listar(Pageable page);
 
 }
--- src/main/java/com/jwiltenburg/transito/domain/service/impl/VeiculoServiceImpl.java
+++ src/main/java/com/jwiltenburg/transito/domain/service/impl/VeiculoServiceImpl.java
@@ -3,9 +3,12 @@ package com.jwiltenburg.transito.domain.service.impl;
 import com.jwiltenburg.transito.api.controller.data.request.VeiculoRequest;
 import com.jwiltenburg.transito.api.controller.data.response.VeiculoResponse;
 import com.jwiltenburg.transito.api.converter.VeiculoConverter;
+import com.jwiltenburg.transito.domain.exception.ResourceNotFoundException;
 import com.jwiltenburg.transito.domain.repository.VeiculoRepository;
 import com.jwiltenburg.transito.domain.service.VeiculoService;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -22,5 +25,14 @@ public class VeiculoServiceImpl implements VeiculoService {
         var veiculoSalvo = veiculoRepository.save(converter.toVeiculoModel(request));
 
         return converter.toVeiculoResponse(veiculoSalvo);
+    }
+
+    @Override
+    public Page<VeiculoResponse> listar(Pageable page) {
+        var pages = veiculoRepository.findAll(page);
+        if(pages.isEmpty()){
+            throw new ResourceNotFoundException("Recurso não encontrado");
+        }
+        return pages.map(converter::toVeiculoResponse);
     }
 }
