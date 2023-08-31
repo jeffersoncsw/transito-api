b9a7dabd133bb8b2ddf0cd18d0d5a847b39d6d9c
“jeffersoncsw” <je_wiltenburg@hotmail.com>
2023-08-31 16:38:29 -0300

    Validating the plate property with Regex

--- src/main/java/com/jwiltenburg/transito/api/controller/data/request/VeiculoRequest.java
+++ src/main/java/com/jwiltenburg/transito/api/controller/data/request/VeiculoRequest.java
@@ -3,6 +3,7 @@ package com.jwiltenburg.transito.api.controller.data.request;
 import com.jwiltenburg.transito.domain.model.Proprietario;
 import com.jwiltenburg.transito.domain.model.StatusVeiculo;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -28,6 +29,7 @@ public class VeiculoRequest implements Serializable {
 
     @NotBlank
     @Size(max = 7)
+    @Pattern(regexp = "[A-Z]{3}[0-9][0-9A-Z][0-9]{2}", message = "A placa precisa estar no formato antigo (XXX0000) ou no atual (XXX0X00).")
     private String placa;
 
     private StatusVeiculo status;
